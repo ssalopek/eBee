ddd24d909cd5ffd73bdccdb16c7f34d6d7635cf8
Juraj Štrekelj <Juraj95@gmail.com>
2020-09-21 00:57:13 +0200

    gsm i lcd prikaz rework dodo prikaz sensor id-a bilo bi dobro dodati tipkalo kojim se mijenja prikaz na LCD-u sa prikaza sensor id-a na iščitavanja podataka senzora.

--- eBee.ino
+++ eBee.ino
@@ -26,7 +26,7 @@ GPRS gprs;
 GSM gsmAccess;
 GSM_SMS sms;
 
-char url[] = "https://localhost:44312" // ovo treba zamjeniti sa URL-om stanice kad se podigne
+char url[] = "https://localhost:44312"; // ovo treba zamjeniti sa URL-om stanice kad se podigne
 
 // DHT sensor variables
 DHT dht(DHTPIN, DHTTYPE);
@@ -38,6 +38,10 @@ float calibration_factor = 782.4;
 
 // LCD display variables
 LiquidCrystal_I2C lcd(0x27, 16, 2); //LCD address -> 0x27, 16 chars, 2 line display
+// stanje prikaza na lcd-u, bilo bi dobro addat tipkalo koje mijenja stanje
+// state == 0 - prikaz sensorId-a
+// state == 1 - prikaz iščitavanja
+int lcdState = 0;
 
 //-- general variables --
 // recieved once setup finishes making inital POST to web service
@@ -58,12 +62,15 @@ void setup() {
 
   // now send a POST request to get SensorId
   while(sensorId.length() <= 0){
-    sensorId = postBeeHive();
+    sensorId = postSensor();
 
     if(sensorId.length() <= 0){
       delay(10000); // wait for 10 seconds before sending another POST request
     }
   }
+  // display sensor id
+  showSensorId();
+  delay(60000); // show sensor Id for the next 60 seconds (better implement switch by button)
   
   // init weight sensor
   scale.begin(DOUT, CLK); //initialize HX711 library with data output pin and clock input pin 
@@ -110,6 +117,9 @@ void loop()
   
   // send Post request to Sensor api route
   postSensorData(sensorData);
+
+  // display data on lcd
+  showReadings(doc["Weight"], doc["Temperature"], doc["Humidity"]);
 
   // turn off components to preserve power
   shutdownGsm();
@@ -267,13 +277,25 @@ bool checkStatusOk()
 }
 
 // funkcije za prikaz na LCD-u
-void showReadings()
+void showSensorId()
 {
+  // first we need to clear the screen
+  lcd.clear();
+
+  lcd.autoscroll();
+  lcd.print(sensorId);
+  lcd.noAutoscroll();
+}
+
+void showReadings(float masa, float temp, float hum)
+{
+  // first we need to clear the screen
+  lcd.clear();
+  
   // weight readings
   lcd.setCursor(0, 0);
   lcd.print("Masa|" );
   lcd.setCursor(0,1);
-
   lcd.print(masa); 
   lcd.setCursor(3,1);
   lcd.print("|g");
