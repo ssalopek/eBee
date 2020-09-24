76e637c263f687eb484673de2b59bebcdb9d4553
Juraj Štrekelj <Juraj95@gmail.com>
2020-09-24 18:01:48 +0200

    reimplementacija pomocu drugog library-ja

--- eBee.ino
+++ eBee.ino
@@ -2,7 +2,8 @@
 #include <DHT.h>
 #include <LiquidCrystal_I2C.h>
 #include <ArduinoJson.h>
-#include <Http.h>
+#include <SoftwareSerial.h>
+#include "SIM800L.h"
 
 #define DELAY_TIME 10000
 
@@ -15,10 +16,15 @@
 #define CLK 3 //load cell SCK pin
 
 // GSM pins and other values
-const char BEARER[] PROGMEM = "internet.ht.hr";
-#define RST_PIN 8
-#define RX_PIN 10
+SIM800L* sim800l;
 #define TX_PIN 9
+#define RX_PIN 10
+#define RST_PIN 8
+const char APN[] = "internet.ht.hr";
+const char URL_SensorData[] = "https://iotebee.azurewebsites.net/api/SensorData";
+const char URL_Sensor[] = "https://iotebee.azurewebsites.net/api/Sensor";
+const char CONTENT_TYPE[] = "application/json";
+//const char PAYLOAD[] = "{\"name\": \"morpheus\", \"job\": \"leader\"}"; --- ovo je kao 'body'
 
 // DHT sensor variables
 DHT dht(DHTPIN, DHTTYPE);
@@ -38,7 +44,7 @@ int lcdState = 0;
 //-- general variables --
 // recieved once setup finishes making inital POST to web service
 // should be stored in local storage (SD card or similar)
-String sensorId = ""; 
+char* sensorId; 
 
 // refer to https://arduinojson.org/v6/example/generator/ for JSON serialization
 // refer to https://arduinojson.org/v6/example/http-client/ for JSON deserialization of HTTP responses from server
@@ -47,14 +53,25 @@ String sensorId = "";
 // use as startup for setting up values
 void setup() {
   Serial.begin(9600); 
+  while(!Serial);
+
+  // Initialize a SoftwareSerial
+  SoftwareSerial* serial = new SoftwareSerial(TX_PIN, RX_PIN);
+  serial->begin(9600);
+  delay(1000);
+
+  // Initialize SIM800L driver with an internal buffer of 200 bytes and a reception buffer of 512 bytes, debug disabled
+  //sim800l = new SIM800L((Stream *)serial, RST_PIN, 200, 512);
+
+  // Equivalent line with the debug enabled on the Serial
+  sim800l = new SIM800L((Stream *)serial, RST_PIN, 200, 512, (Stream *)&Serial);
+
+  // Setup module for GPRS communication
+  setupModule();
 
   // now send a POST request to get SensorId
-  while(sensorId.length() <= 0){
-    sensorId = postSensor();
-
-    if(sensorId.length() <= 0){
-      delay(10000); // wait for 10 seconds before sending another POST request
-    }
+  while(!postSensor()){
+    delay(10000); // wait for 10 seconds before sending another POST request   
   }
   
   // display sensor id
@@ -131,102 +148,174 @@ float readWeight()
 }
 
 // funkcije za GSM i komunikaciju s web servisom
-HTTP connectHttp(int *success)
-{
-  HTTP http(9600, RX_PIN, TX_PIN, RST_PIN);
-  Result result = http.connect(BEARER);
-  Serial.print(F("HTTP connect: "));
-  Serial.println(result); // result bi trebo biti 0 što znači SUCCESS po njegovim Enumima
-	
-  if (result == SUCCESS)
-  {
-    Serial.println(response);
-  	*success = 0;
+void setupModule() {
+    // Wait until the module is ready to accept AT commands
+  while(!sim800l->isReady()) {
+    Serial.println(F("Problem to initialize AT command, retry in 1 sec"));
+    delay(1000);
   }
-  else
-  {
-	Serial.println(F("Failed to connect."));
-	*success = 1;
-  	delay(1000);
+  Serial.println(F("Setup Complete!"));
+
+  // Wait for the GSM signal
+  uint8_t signal = sim800l->getSignal();
+  while(signal <= 0) {
+    delay(1000);
+    signal = sim800l->getSignal();
   }
-	
-  return http;
+  Serial.print(F("Signal OK (strenght: "));
+  Serial.print(signal);
+  Serial.println(F(")"));
+  delay(1000);
+
+  // Wait for operator network registration (national or roaming network)
+  NetworkRegistration network = sim800l->getRegistrationStatus();
+  while(network != REGISTERED_HOME && network != REGISTERED_ROAMING) {
+    delay(1000);
+    network = sim800l->getRegistrationStatus();
+  }
+  Serial.println(F("Network registration OK"));
+  delay(1000);
+
+  // Setup APN for GPRS configuration
+  bool success = sim800l->setupGPRS(APN);
+  while(!success) {
+    success = sim800l->setupGPRS(APN);
+    delay(5000);
+  }
+  Serial.println(F("GPRS config OK"));
 }
 
-void disconnectHttp(HTTP http)
+bool connectGprs()
 {
-  Serial.print(F("HTTP disconnect: "));
-  Serial.print(http.disconnect());
-}
-
-void postSensorData(char* body)
-{
-  int success = 1;
-  do
-  {
-  	HTTP http = connectHttp(&success);
-  }while(success == 1);
-  
-  char response[58];
-  Result result = http.post("https://iotebee.azurewebsites.net/api/SensorData", body, response);
-  Serial.print(F("HTTP POST: "));
-  Serial.println(result);
-  if (result == SUCCESS)
-  {
-    Serial.println(response);
+  // Go into low power mode
+  bool normalPowerMode = sim800l->setPowerMode(NORMAL);
+  if(normalPowerMode) {
+    Serial.println(F("Module in normal power mode"));
+  } else {
+    Serial.println(F("Failed to switch module to normal power mode"));
   }
   
-  disconnectHttp(http);
+  // Establish GPRS connectivity (5 trials)
+  bool connected = false;
+  for(uint8_t i = 0; i < 5 && !connected; i++) {
+    delay(1000);
+    connected = sim800l->connectGPRS();
+  }
+
+  // Check if connected, if not reset the module and setup the config again
+  if(connected) {
+    Serial.println(F("GPRS connected !"));
+  } else {
+    Serial.println(F("GPRS not connected !"));
+    Serial.println(F("Reset the module."));
+    sim800l->reset();
+    setupModule();
+  }
+  
+  return connected;
+}
+
+bool disconnectGprs()
+{
+  // Close GPRS connectivity (5 trials)
+  bool disconnected = sim800l->disconnectGPRS();
+  for(uint8_t i = 0; i < 5 && !disconnected; i++) {
+    delay(1000);
+    disconnected = sim800l->disconnectGPRS();
+  }
+  
+  if(disconnected) {
+    Serial.println(F("GPRS disconnected !"));
+  } else {
+    Serial.println(F("GPRS still connected !"));
+  }
+
+  // Go into low power mode
+  bool lowPowerMode = sim800l->setPowerMode(MINIMUM);
+  if(lowPowerMode) {
+    Serial.println(F("Module in low power mode"));
+  } else {
+    Serial.println(F("Failed to switch module to low power mode"));
+  }
+
+  return disconnected;
+}
+
+void postSensorData(char *body)
+{
+  while(!connectGprs());
+
+  Serial.println(F("Start HTTP POST..."));
+
+  // Do HTTP POST communication with 10s for the timeout (read and write)
+  uint16_t rc = sim800l->doPost(URL_SensorData, CONTENT_TYPE, *body, 10000, 10000);
+  if(rc == 200) {
+    // Success, output the data received on the serial
+    Serial.print(F("HTTP POST successful ("));
+    
+    uint8_t bytesRecv = sim800l->getDataSizeReceived()
+    Serial.print(bytesRecv);
+    
+    Serial.println(F(" bytes)"));
+    Serial.print(F("Received : "));
+
+    const char* dataRecv = sim800l->getDataReceived();
+    Serial.println(dataRecv);
+  } else {
+    // Failed...
+    Serial.print(F("HTTP POST error "));
+    Serial.println(rc);
+  }
+  
+  while(!disconnectGprs());
+  
   return;
 }
 
 // sends a post request which will add this sensor to the Sensor Database Table and return the Id of the sensor
 // can return empty string "" or the Id of the sensor in the table
 // the Id should be displayed on the LCD screen
-String postSensor()
-{
-  Result result;
-  
-  int success = 1;
-  do
-  {
-  	HTTP http = connectHttp(&success);
-  }while(success == 1);
-  
-  result = http.get("https://iotebee.azurewebsites.net/api/Sensor", response);
-  Serial.print(F("HTTP GET: "));
-  Serial.println(result);
-  if (result == SUCCESS)
-  {
-    Serial.println(response);
-  }
-  
-  char response[58]; // očekujemo 58 charactera da dobijemo nazad od servera
-  result = http.post("https://iotebee.azurewebsites.net/api/Sensor", "{}", response);
-  Serial.print(F("HTTP POST: "));
-  Serial.println(result);
-  if (result == SUCCESS)
-  {
-    Serial.println(response);
-    StaticJsonBuffer<58> jsonBuffer;
-    JsonObject &root = jsonBuffer.parseObject(response);
+bool postSensor()
+{ 
+  while(!connectGprs());
 
-    String id = root[F("SensorId")];
+  Serial.println(F("Start HTTP POST..."));
+
+  bool postSuccess = false;
+  // Do HTTP POST communication with 10s for the timeout (read and write)
+  uint16_t rc = sim800l->doPost(URL_Sensor, CONTENT_TYPE, PAYLOAD, 10000, 10000);
+  if(rc == 200) {
+    // Success, output the data received on the serial
+    Serial.print(F("HTTP POST successful ("));
+    
+    uint8_t bytesRecv = sim800l->getDataSizeReceived()
+    Serial.print(bytesRecv);
+    
+    Serial.println(F(" bytes)"));
+    Serial.print(F("Received : "));
+
+    const char* dataRecv = sim800l->getDataReceived();
+    Serial.println(dataRecv);
+
+    const size_t capacity = JSON_OBJECT_SIZE(1) + 60;
+    DynamicJsonDocument doc(capacity);
+    
+    deserializeJson(doc, dataRecv);
+
+    sensorId = doc["SensorId"];
+    
     Serial.print(F("ID: "));
-    Serial.println(id);
-
-    disconnectHttp(http);
-    return id;
+    Serial.println(sensorId);
+    postSuccess = true;
+  } else {
+    // Failed...
+    Serial.print(F("HTTP POST error "));
+    Serial.println(rc);
   }
   
-  disconnectHttp(http);
-  return "";
-  // 3. Read the Json response containing the SensorId and deserialize the json
-  //const size_t capacity = JSON_OBJECT_SIZE(1) + 60;
-  //DynamicJsonDocument doc(capacity);
-  
-  //deserializeJson(doc, client);
-  
+  while(!disconnectGprs());
+
+  return postSuccess;
 }
 
 // funkcije za prikaz na LCD-u
